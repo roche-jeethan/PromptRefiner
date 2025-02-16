f543ffc6adf2743e32860c9fa8dcb0d9e6c609ae
Jeethan Roche <jeethanroche@gmail.com>
2025-02-16 13:25:29 +0530

    fix/footer: added updated year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="w-full px-6 py-4 flex justify-center items-center bg-gray-200 dark:bg-gray-800">
         <p className="text-gray-700 dark:text-gray-300">
-            © 2021 Prompt Refiner. All rights reserved.
+            © {new Date().getFullYear()} Prompt Refiner. All rights reserved.
         </p>
         </footer>
     );
