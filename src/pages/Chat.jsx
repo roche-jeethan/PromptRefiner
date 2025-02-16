1be49d1716c0b51ab28048dc92e7a86e714b9525
Jeethan Roche <jeethanroche@gmail.com>
2025-02-16 12:10:21 +0530

    refactor: updated texts in chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Chat() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen bg-white dark:bg-gray-800">
             <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100">
                 Chat
             </h1>
