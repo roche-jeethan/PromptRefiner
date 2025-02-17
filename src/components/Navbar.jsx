0f4d16494eb587053d631f12ccfad2fefb4d4d3c
Jeethan Roche <jeethanroche@gmail.com>
2025-02-17 20:10:19 +0530

    refact/navbar: added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import useDarkMode  from "../hooks/useDarkMode";
+import icon from "../assets/logo192.png";
 
 function Navbar() {
   const [darkMode, setDarkMode] = useDarkMode();
 
   return (
     <nav className="w-full px-6 py-4 flex justify-between items-center bg-white-200 dark:bg-gray-900">
-      <Link to="/" className="text-2xl font-bold text-gray-900 dark:text-gray-100">
-        Prompt Refiner
-      </Link>
+      <div className="flex items-center">
+        <img src={icon} alt="Logo" className="w-8 h-8 mr-2" />
+        <Link to="/" className="text-2xl font-bold text-gray-900 dark:text-gray-100">
+          Prompt Refiner
+        </Link>
+      </div>
       <button
         onClick={() => setDarkMode(!darkMode)}
         className="p-2 rounded bg-gray-300 dark:bg-white-700"
