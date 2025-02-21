07293b4cf0bd05988daa19196ba814dad12785f9
Jeethan Roche <jeethanroche@gmail.com>
2025-02-21 20:58:23 +0530

    styles/background: added styles inside app.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import Footer from "./components/Footer";
+import bgWhite from "./assets/bg-white.svg";
+import bgDark from "./assets/bg-dark.svg";
 
 function App() {
   return (
       <Router>
-        <div className="flex flex-col min-h-screen">
+        <div className={`flex flex-col min-h-screen ${bgWhite} dark:${bgDark} bg-pattern bg-fixed bg-cover`}>
           <Navbar />
           <main className="flex-grow">
             <Routes>
