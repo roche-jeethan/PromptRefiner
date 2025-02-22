ee46943024065e58d6e08ba031af75b56b1797b9
Jeethan Roche <jeethanroche@gmail.com>
2025-02-22 19:45:42 +0530

    style/app: removed previous styles due to bug

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import Footer from "./components/Footer";
-import bgWhite from "./assets/bg-white.svg";
-import bgDark from "./assets/bg-dark.svg";
 
 function App() {
   return (
       <Router>
-        <div className={`flex flex-col min-h-screen ${bgWhite} dark:${bgDark} bg-pattern bg-fixed bg-cover`}>
+        <div className={"flex flex-col min-h-screen dark:bg-gray-900 bg-pattern bg-fixed bg-cover"}>
           <Navbar />
           <main className="flex-grow">
             <Routes>
