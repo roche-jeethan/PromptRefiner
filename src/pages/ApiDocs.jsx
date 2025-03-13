906f869117ef7b2ceeb30fbb93bb81db48f769ab
roche-jeethan <jeethanroche@gmail.com>
2025-03-13 19:11:26 +0530

    feat/docs: adding content

--- src/pages/ApiDocs.jsx
+++ src/pages/ApiDocs.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function ApiDocs() {
     return(
-        <div className="flex flex-col items-center h-screen bg-bgWhite dark:bg-bgDark">
-            <p>Testing sentence</p>
+        <div className="flex flex-col h-screen bg-bgWhite text-white dark:bg-bgDark">
+            <div className="ml-10">
+            <h1 className="text-4xl mt-3">Guidelines</h1>
+            <p className=""></p>
+            </div>
         </div>
     );
 };
